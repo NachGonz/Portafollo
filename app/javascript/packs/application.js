ae5cf9fd3bf45dfb19a70867f3b35d1b4d6985dd
NachGonz <j.gonzalez@outlook.cl>
2019-10-04 11:32:32 -0400

    proyecto commit 1

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require bootstrap
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :clientes
+  devise_for :users
+  
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contacto'
+
+  root :to=> 'welcome#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
